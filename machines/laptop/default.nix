83f0fc82b848d717d41eaffc43aae912dcacf2f3
nervous-fish <dev@nervous.fish>
2023-05-19 21:02:45 +0200

    feat: remove intel kernelparams

--- machines/laptop/default.nix
+++ machines/laptop/default.nix
@@ -7,7 +7,7 @@
     ../../common/configuration.nix
   ];
 
-  boot.kernelParams = [ "intel_idle.max_cstate=1" "i915.enable_psr=0" "quiet" "module_blacklist=nouveau" ];
+  boot.kernelParams = [ "quiet" "module_blacklist=nouveau" ];
   systemd.watchdog.rebootTime = "0";
   
   nixpkgs.config.allowUnfree = true;
