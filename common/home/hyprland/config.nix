4ca9fcd49261786319885b4428898b6741edab45
nervous-fish <dev@nervous.fish>
2023-05-19 15:35:08 +0200

    fix: autostart dunst

--- common/home/hyprland/config.nix
+++ common/home/hyprland/config.nix
@@ -8,7 +8,7 @@
     # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
     # Execute your favorite apps at launch
-    exec-once = hyprpaper & waybar
+    exec-once = hyprpaper & waybar & dunst
 
     # Source a file (multi-file configs)
     # source = ~/.config/hypr/myColors.conf
