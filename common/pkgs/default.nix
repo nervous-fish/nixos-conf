c5e7cda30e339f381a3ec5ece008e180e56f828b
nervousfish <119760848+nervous-fish@users.noreply.github.com>
2024-02-07 12:22:37 +0100

    remove unused stuff

--- common/pkgs/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  environment.systemPackages = let themes = pkgs.callPackage ./sddm-themes.nix { }; in [
-    pkgs.libsForQt5.qt5.qtgraphicaleffects
-    pkgs.libsForQt5.qt5.qtsvg
-    pkgs.libsForQt5.qt5.qtquickcontrols2
-    themes.nosddm
-  ];
-}
