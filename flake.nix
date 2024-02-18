a1f35304eafde8a546fe8ce98878d87923c55efb
nervousfish <119760848+nervous-fish@users.noreply.github.com>
2024-02-18 10:42:56 +0100

    stable

--- flake.nix
+++ flake.nix
@@ -1,4 +1,13 @@
 {
+  #inputs = {
+  #  nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  #  
+  #  home-manager = {
+  #    url = "github:nix-community/home-manager/release-23.11";
+  #    inputs.nixpkgs.follows = "nixpkgs";
+  #  }; 
+  #
+  #};
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     
@@ -10,7 +19,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, sops-nix, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -44,9 +53,6 @@
                   ./machines/desktop/home
                 ];
               };
-              sharedModules = [
-                sops-nix.homeManagerModules.sops
-              ];
             };
           }
         ];
