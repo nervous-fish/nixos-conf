08dd27298d8d8b0fe7111e7d1f3331f8fe3af2ec
nervousfish <119760848+nervous-fish@users.noreply.github.com>
2025-04-27 13:43:10 +0200

    move kernel to its own nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,18 +2,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+    nixpkgs-kernel.url = "github:nixos/nixpkgs/cdd2ef009676ac92b715ff26630164bb88fec4e0";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };  
   };
 
-  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, nixpkgs-unstable, nixpkgs-kernel, home-manager, ... }: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
         specialArgs = {
           pkgs-unstable = nixpkgs-unstable.legacyPackages.${system}; # alias unstable packages to this variable
+          pkgs-kernel = nixpkgs-kernel.legacyPackages.${system}; # alias unstable packages to this variable
         };
         modules = [
           ./machines/desktop
