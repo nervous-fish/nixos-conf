7fbf0b93fc4388eb02409cf039b32951971f4608
nervousfish <119760848+nervous-fish@users.noreply.github.com>
2025-05-05 10:08:30 +0200

    remove cascade

--- common/users/nervousfish/home/firefox/default.nix
+++ common/users/nervousfish/home/firefox/default.nix
@@ -2,13 +2,5 @@
 {
   programs.firefox = {
     enable = true;
-    # policies = {
-    #   DisableProfileCustomizations = true;
-    # };
-    # profiles.nervousfish = {
-    #   # extraConfig = builtins.readFile "${package}/user.js";
-    #   # containersForce = true;
-    #   userChrome = lib.mkBefore (builtins.readFile ./userChrome.css);
-    # };
   };
 }
