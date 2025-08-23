ccd04361487d195383c00e1de304d0eecb69d57b
nervousfish <119760848+nervous-fish@users.noreply.github.com>
2025-08-23 14:03:58 +0200

    no more acs override

--- machines/desktop/hardware.nix
+++ machines/desktop/hardware.nix
@@ -17,39 +17,18 @@
       };
     };
     supportedFilesystems = [ "ntfs" ];
-    
+
     kernelPackages = pkgs-kernel.linuxPackagesFor pkgs-kernel.linux_6_12;
 
     initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-    initrd.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ];    
-    initrd.preDeviceCommands = ''
-      DEVS="0000:11:00.0"
-
-      for DEV in $DEVS; do
-        echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
-      done
-      modprobe -i vfio-pci
-    '';
+    initrd.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ];
 
     kernelModules = [ "kvm-amd" ];
 
     kernelParams = [
       "quiet"
       "iommu=pt"
-      "vfio-pci.ids=10de:1b06,10de:10ef,1022:43f6,1022:15b6,168c:0030"
-      "pcie_acs_override=downstream"
-    ];
-
-    kernelPatches = [
-      {
-        name = "add-acs-overrides";
-        patch = pkgs-kernel.fetchurl {
-          name = "add-acs-overrides.patch";
-          url =
-            "https://aur.archlinux.org/cgit/aur.git/plain/0001-add-acs-overrides.patch?h=linux-vfio&id=1a1a7ab832756f4fc4e93f569d541127e90bcdd3";
-          sha256 = "bdd2a5a56e01e91723907afb40d28bed77b7d5107aba92c85adb3ce6967e713a";
-        };
-      }
+      "vfio-pci.ids=10de:1b06,10de:10ef,1022:15b6"
     ];
 
     extraModulePackages = [ ];
@@ -64,15 +43,15 @@
     { device = "/dev/disk/by-uuid/781D-4308";
       fsType = "vfat";
     };
-  
-  fileSystems."/run/media/nervousfish/nvme1m1p1" = {
-    device = "/dev/disk/by-label/nvme1n1p1";
+
+  fileSystems."/run/media/nervousfish/floppy1" = {
+    device = "/dev/disk/by-uuid/fd5af435-58da-461e-9522-c7943f528758";
   };
 
-  fileSystems."/run/media/nervousfish/nvme2m1p1" = {
-    device = "/dev/disk/by-label/nvme2n1p1";
+  fileSystems."/run/media/nervousfish/floppy2" = {
+    device = "/dev/disk/by-uuid/2930271e-7a79-4605-aa75-2d694c10544b";
   };
-  
+
   swapDevices = [ ];
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
